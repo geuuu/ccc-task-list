b91fd94e5bdb6620cd507a23d0abb79596200b96
Usuario invitado <Guest@MacBook-Air-de-Stephanie.local>
2018-02-10 14:32:19 -0600

    Task list

--- js/task-list.js
+++ js/task-list.js
@@ -1,19 +1,27 @@
+var categorias = [
+  {'value':'success','label':'Verduras'},
+  {'value':'warning','label':'Comida chatarra'},
+  {'value':'info','label':'Frutas'},
+  {'value':'secondary','label':'Alcohol'}
+];
 $(document).ready(function(){
   var $formulario_de_lista = $('#formulario-de-lista');
   var $lista_principal = $('#lista-principal');
-  var $tarea = $('input#tarea');
-
+  var $producto = $('input#producto');
+  var $categoria = $('select#categoria');
   var template = '<li class="list-group-item list-group-item-action"></li>';
 
   $formulario_de_lista.on('submit', function( event ){
     event.preventDefault();
-    var texto_de_tarea = $tarea.val().trim();
-
-    if( texto_de_tarea ){
-
+    var texto_de_producto = $producto.val().trim();
+    var categoria = $categoria.find('option:selected').index();
+    //alert(texto_de_producto);
+    //alert(categoria);
+    if( texto_de_producto ){
+      var clase = categorias [ categoria ];
       var $new_row = $(
-        '<li class="list-group-item list-group-item-action">' +
-      texto_de_tarea +
+        '<li class="list-group-item bg-'+clase.value+' list-group-item-action">' +
+      texto_de_producto +
       '</li>'
     );
       $lista_principal.append( $new_row );
@@ -21,7 +29,7 @@ $(document).ready(function(){
       $new_row.hide().fadeIn();
     }
 
-    $tarea.val('');
+    $producto.val('');
 
     // alert(texto_de_tarea )
   })
